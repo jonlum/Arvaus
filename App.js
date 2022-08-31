6215a45f9abff6fae919ff309d352f0c03baf5f7
Joni Lumpin <joni.lumpin@gmail.com>
2022-08-31 23:44:06 +0300

    Arvaus palautus

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Alert, TextInput, Text  } from 'react-native';
 
 export default function App() {
+  const [guess, setGuess] = useState(0);
+  
+   
+  
+
+  const go = () => { 
+  const number = Math.floor(Math.random() * 100) + 1;
+    
+    if (guess == number) {
+      return 'Voitto';
+    else if (condition) {
+      
+    } else {
+      
+    }(guess < number) {
+
+    } 
+
+    }
+  };
+ 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <><View style={styles.container}>
+      <Text>Your guess {guess}</Text>
+      <Text></Text> 
+      <TextInput style={styles.input} keyboardType= 'number-pad' onChangeText={guess => setGuess(guess)} value={guess} />
       <StatusBar style="auto" />
     </View>
+    <View style={styles.container2}>
+        <Button onPress={go} title="Make a guess" />
+        <StatusBar style="auto" />
+      </View></>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  image : {
+    width: 250,
+    height: 100
+  },
+  input : {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1
+  }
 });
